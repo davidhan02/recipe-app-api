8a3dd44083c59569fbd92d821882a5f90d77fdca
David <davidhan.5.25@gmail.com>
2019-08-12 21:14:03 -0700

    Testing mocking and admin API

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^-ra)h1v(2o-lr(h5yom*28-*_)(71!7s0l&!kd$hkka+-dn_v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.99.100']
 
 
 # Application definition
